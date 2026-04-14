b28c36f884f2fa22a394b04360e21630e41a4543
Lukmon Adeokun <adekunledally@gmail.com>
2026-04-14 19:01:58 +0100

    third CI CD modification for a calculator app

--- build/lib/myapp/calculator.py
+++ build/lib/myapp/calculator.py
@@ -1,16 +1,16 @@
 def add(a: int, b: int) -> int:
-    return a+b
+    return a + b
 
 
 def subtract(a: int, b: int) -> int:
-    return a-b
+    return a - b
 
 
 def multiply(a: int, b: int) -> int:
-    return a*b
+    return a * b
 
 
 def divide(a: int, b: int) -> float:
     if b == 0:
         raise ValueError("Cannot Divide by Zero")
-    return a/b
+    return a / b
